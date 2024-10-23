af004495588ee2c2d6ba63a1a258cce0a544412b
Manaswini Sasmal <98816752+Monisasmal@users.noreply.github.com>
2024-10-23 14:22:22 +0530

    Simple Project using HTML, CSS, JS

---
+++ index.js
@@ -0,0 +1,11 @@
+let result = document.getElementById('testing');
+result.style.backgroundColor = "Yellow";
+result.style.height = "200px";
+result.style.width = "300px";
+result.style.padding = '10px';
+result.style.margin = '20px';
+result.style.fontSize = '18px';
+result.style.fontWeight = "bold";
+result.style.justifyContent = "center";
+result.style.alignItems = "center";
+result.style.display = "flex";
